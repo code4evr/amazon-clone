48098ec5a84d41a5d213cc1449398f67da00f8cc
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2021-01-16 21:46:49 +0530

    firebase config file added

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBmXncUgcLChQcBpdtXsL0GOIoVvecQCk4",
+  authDomain: "clone-8ca25.firebaseapp.com",
+  projectId: "clone-8ca25",
+  storageBucket: "clone-8ca25.appspot.com",
+  messagingSenderId: "161305785117",
+  appId: "1:161305785117:web:27a651ae8ab1cadda6e7f4",
+  measurementId: "G-090B02Z3GY"
+};
